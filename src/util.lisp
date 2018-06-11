6c437c4ddedf31074df549c8f81b465f0a290ce8
Emil Tomov <emil.tomov@rewe-digital.com>
2018-06-11 16:27:38 +0300

    Adding more utils funcs from On lisp book

--- src/util.lisp
+++ src/util.lisp
@@ -1,3 +1,5 @@
+;;These functions utils are from
+;;OnLisp book of Paul Graham
 (in-package #:foo-lisp)
 
 ;;misc
@@ -182,3 +184,132 @@
                  (intern (make-string 1
                                       :initial-element c)))
        (symbol-name sym)))
+
+;;function builders
+(defun memoize (fn)
+  "Memoize returned result from function.Works with functions that return single values"
+  (let ((cache (make-hash-table :test #'equal)))
+    #'(lambda (&rest args)
+        (multiple-value-bind (val win) (gethash args cache)
+          (if win
+              val
+              (setf (gethash args cache)
+                    (apply fn args)))))))
+
+(defun compose (&rest fns)
+  "Composes functions, all butlast must be single argument functions"
+  (if fns
+      (let ((fn1 (car (last fns)))
+            (fns (butlast fns)))
+       #'(lambda (&rest args)
+            (reduce#'funcall fns
+                      :from-end t
+                      :initial-value (apply fn1 args))))
+     #'identity))
+
+(defun fif (if then &optional else)
+  "Function if
+   Makes: (mapcar#'(lambda (x)
+            (if (slave x)
+                (owner x)
+                (employer x)))
+          people)
+
+   To: (mapcar (fif#'slave#'owner#'employer) people)"
+ #'(lambda (x)
+      (if (funcall if x)
+          (funcall then x)
+          (if else (funcall else x)))))
+
+(defun fint (fn &rest fns)
+  "Function intersection.
+   Makes: (find-if#'(lambda (x)
+            (and (signed x) (sealed x) (delivered x)))
+           docs)
+
+   To: (find-if (fint#'signed#'sealed#'delivered) docs)"
+  (if (null fns)
+      fn
+      (let ((chain (apply#'fint fns)))
+       #'(lambda (x)
+            (and (funcall fn x) (funcall chain x))))))
+
+(defun fun (fn &rest fns)
+  "Function union.Same like fint but uses 'or' instead of 'and'"
+  (if (null fns)
+      fn
+      (let ((chain (apply#'fun fns)))
+       #'(lambda (x)
+           (or (funcall fn x) (funcall chain x))))))
+
+;;Function to define flat list recursers.
+(defun lrec (rec &optional base)
+  "Function to define flat list recursers.
+   The first argument to lrec must be a function of two arguments: the current
+   car of the list, and a function which can be called to continue the recursion.
+
+;copy-list
+ (lrec#'(lambda (x f) (cons x (funcall f))))
+
+;remove-duplicates                    
+ (lrec#'(lambda (x f) (adjoin x (funcall f))))
+
+;find-if, for some function fn         
+ (lrec#'(lambda (x f) (if (fn x) x (funcall f))))
+
+;some, for some function fn            
+ (lrec#'(lambda (x f) (or (fn x) (funcall f))))"
+  (labels ((self (lst)
+             (if (null lst)
+                 (if (functionp base)
+                     (funcall base)
+                     base)
+                 (funcall rec (car lst)
+                         #'(lambda ()
+                              (self (cdr lst)))))))
+   #'self))
+
+
+;;functions for traversing trees
+(defun rfind-if (fn tree)
+  "Generalized find-if for trees.Our rfind-if takes the former approach, 
+   so the caller can assume that the function given as the first argument will only be
+   called on atoms"
+  (if (atom tree)
+      (and (funcall fn tree) tree)
+      (or (rfind-if fn (car tree))
+          (if (cdr tree) (rfind-if fn (cdr tree))))))
+
+(defun ttrav (rec &optional (base#'identity))
+  "Tree traverse.
+   1. In the base case it returns its argument.
+   2. In the recursive case, it applies cons to the recursions down the left (car)
+   and right (cdr) subtrees."
+  (labels ((self (tree)
+             (if (atom tree)
+                 (if (functionp base)
+                     (funcall base tree)
+                     base)
+                 (funcall rec (self (car tree))
+                          (if (cdr tree)
+                              (self (cdr tree)))))))
+   #'self))
+
+
+(defun trec (rec &optional (base#'identity))
+  "The second arg to trec should be a function of three arguments: the current object and the two
+   recursers. The latter two will be closures representing the recursions down the
+   left and right subtrees."
+  (labels
+      ((self (tree)
+         (if (atom tree)
+             (if (functionp base)
+                 (funcall base tree)
+                 base)
+             (funcall rec tree
+                     #'(lambda ()
+                          (self (car tree)))
+                       #'(lambda ()
+                            (if (cdr tree)
+                                (self (cdr tree))))))))
+   #'self))
